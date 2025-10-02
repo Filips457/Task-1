617ee5b79c46ef9315034d54fe27cb98d545866d
Filips457 <artemfilipson@gmail.com>
2025-10-02 13:04:16 +0300

    Calculator 1.0 beta
    Simple console calculator with 4 operators

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,44 @@
+﻿namespace Calculator;
+
+public class Calculator
+{
+    private double firstOperand;
+    private double secondOperand;
+    private Func<double, double, double> calculateDelegate;
+
+    private void GetOperator(char operatorCh)
+    {
+        switch (operatorCh)
+        {
+            case '+':
+                calculateDelegate = (a, b) => a + b;
+                break;
+            case '-':
+                calculateDelegate = (a, b) => a - b;
+                break;
+            case '*':
+                calculateDelegate = (a, b) => a * b;
+                break;
+            case '/':
+                calculateDelegate = (a, b) => a / b;
+                break;
+            default:
+                throw new ArgumentException("Не найден оператор!");
+        }
+        Console.WriteLine(operatorCh);
+    }
+
+    public double Calculate()
+    {
+        Console.Write("Введите первый операнд: ");
+        firstOperand = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите оператор: ");
+        GetOperator(Console.ReadKey(true).KeyChar);
+
+        Console.Write("Введите второй операнд: ");
+        secondOperand = double.Parse(Console.ReadLine());
+
+        return calculateDelegate(firstOperand, secondOperand);
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator;
+
+public class Program
+{
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+        do
+        {
+            try
+            {
+                var res = calculator.Calculate();
+                Console.WriteLine($"Результат: {res}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Ошибка деления на ноль: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Ошибка ввода: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.WriteLine("Воспользоваться ещё раз калькулятором? (y/n)");
+        } while (Console.ReadKey(true).Key == ConsoleKey.Y);
+    }
+}
